4c67dc8e166e836401806f320d26c5193adeb610
Oleksandr Maslov <oleksandr.tidal@gmail.com>
2025-04-11 02:10:58 +0200

    Update input_processor_accel.c

--- src/input_processor_accel.c
+++ src/input_processor_accel.c
@@ -2,6 +2,9 @@
 #include <zephyr/device.h>
 #include <drivers/input_processor.h>
 #include <zephyr/dt-bindings/input/input-event-codes.h>
+#include <stdlib.h> 
+
+#define DT_DRV_COMPAT zmk_input_processor_acceleration
 
 /* Maximum number of event codes this processor can handle (e.g. REL_X, REL_Y). */
 #define ACCEL_MAX_CODES 4
@@ -27,6 +30,32 @@ struct accel_data {
     uint16_t last_code;                    /* Last event code processed (e.g. REL_X or REL_Y) */
     int16_t remainders[ACCEL_MAX_CODES];   /* Remainder values for fractional movements per code */
 };
+
+/* Populate config and data for each instance from devicetree */
+#define ACCEL_INST_INIT(inst)                                                    \
+static const uint16_t accel_codes_##inst[] = { INPUT_REL_X, INPUT_REL_Y };      \
+static const struct accel_config accel_config_##inst = {                     \
+    .input_type = DT_INST_PROP_OR(inst, input_type, INPUT_EV_REL),         \
+    .codes = accel_codes_##inst,                                               \
+    .codes_count = 2,                                                          \
+    .track_remainders = DT_INST_NODE_HAS_PROP(inst, track_remainders),         \
+    .min_factor = DT_INST_PROP_OR(inst, min_factor, 1000),                    \
+    .max_factor = DT_INST_PROP_OR(inst, max_factor, 3500),                    \
+    .speed_threshold = DT_INST_PROP_OR(inst, speed_threshold, 1000),           \
+    .speed_max = DT_INST_PROP_OR(inst, speed_max, 6000),                       \
+    .acceleration_exponent = DT_INST_PROP_OR(inst, acceleration_exponent, 1)   \
+};                                                                             \
+static struct accel_data accel_data_##inst = {0};                             \
+DEVICE_DT_INST_DEFINE(inst,                                                    \
+                      NULL,                                                    \
+                      NULL,                                                    \
+                      &accel_data_##inst,                                     \
+                      &accel_config_##inst,                                   \
+                      POST_KERNEL,                                            \
+                      CONFIG_ZMK_INPUT_PROCESSOR_INIT_PRIORITY,               \
+                      &(const struct zmk_input_processor_driver_api){         \
+                          .handle_event = accel_handle_event                  \
+                      });
 
 /* Instantiate for each DT node matching our compatible */
 DT_INST_FOREACH_STATUS_OKAY(ACCEL_INST_INIT)
@@ -149,31 +178,4 @@ static int accel_handle_event(const struct device *dev, struct input_event *even
 
     return 0;
 }
-
-/* Forward declaration of event handler function */
-static int accel_handle_event(const struct device *dev, struct input_event *event,
-    uint32_t param1, uint32_t param2,
-    struct zmk_input_processor_state *state);
-
-/* Populate config and data for each instance from devicetree */
-#define ACCEL_INST_INIT(inst)                                                    \
-static const uint16_t accel_codes_##inst[] = { INPUT_REL_X, INPUT_REL_Y };      \
-static const struct accel_config accel_config_##inst = {                     \
-    .input_type = DT_INST_PROP_OR(inst, type, INPUT_EV_REL),                       \
-    .codes = accel_codes_##inst,                                               \
-    .codes_count = 2,                                                          \
-    .track_remainders = DT_INST_NODE_HAS_PROP(inst, track_remainders),         \
-    .min_factor = DT_INST_PROP_OR(inst, min_factor, 1000),   /* default 1.0 */  \
-    .max_factor = DT_INST_PROP_OR(inst, max_factor, 3500),   /* default 3.5 */ \
-    .speed_threshold = DT_INST_PROP_OR(inst, speed_threshold, 1000),           \
-    .speed_max = DT_INST_PROP_OR(inst, speed_max, 6000),                       \
-    .acceleration_exponent = DT_INST_PROP_OR(inst, acceleration_exponent, 1)   \
-};                                                                             \
-static struct accel_data accel_data_##inst;                                    \
-DEVICE_DT_INST_DEFINE(inst, NULL, NULL,                                        \
-                      &accel_data_##inst, &accel_config_##inst,                \
-                      APPLICATION, CONFIG_ZMK_INPUT_PROCESSOR_INIT_PRIORITY,   \
-                      &(const struct zmk_input_processor_driver_api){          \
-                          .handle_event = accel_handle_event                   \
-                      })
 
